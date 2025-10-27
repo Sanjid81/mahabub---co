f0b2e4caaf05fa016fcd55733cb6f57a04605311
Sanjida Akter <youremail@example.com>
2025-10-27 17:17:15 +0600

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,19 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/scripts/index.js",
-  },
+  entry: "./src/scripts/index.js",
   mode: process.env.MODE || "development",
   output: {
-    filename: "[name].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: "images/[name][ext]",
     clean: true,
   },
   module: {
     rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: { presets: ["@babel/preset-env"] },
-        },
-      },
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
@@ -34,16 +25,20 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
     new HtmlWebpackPlugin({
-      template: "./index.html", // ← root folder
+      template: "./index.html",
       filename: "index.html",
-      inject: "body",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" }, // ✅ images copy হবে dist/assets এ
+      ],
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"), // serve dist folder
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 3000,
