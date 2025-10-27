d922cfb6230fe3141ae3c4e5112183a865a2f287
Sanjida Akter <youremail@example.com>
2025-10-27 15:54:08 +0600

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,53 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const config = {
+module.exports = {
   entry: {
     app: "./src/scripts/index.js",
   },
-  mode: process.env.MODE,
-  target: "web",
+  mode: process.env.MODE || "development",
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
+          options: { presets: ["@babel/preset-env"] },
         },
       },
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new HtmlWebpackPlugin({
+      template: "./index.html", // ← root folder
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // serve dist folder
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
 };
-
-module.exports = config;
